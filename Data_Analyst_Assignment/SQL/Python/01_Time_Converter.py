6a9363600e262f9ccdcc1ea2fc68f8eb7b082f30
Harshitha Thangella <harshithathangella24@gmail.com>
2026-04-09 11:58:01 +0530

    Delete Data_Analyst_Assignment/SQL/Python directory

--- Data_Analyst_Assignment/SQL/Python/01_Time_Converter.py
+++
@@ -1,7 +0,0 @@
-def convert_minutes(minutes):
-    hrs = minutes // 60
-    mins = minutes % 60
-    print(str(hrs) + " hrs " + str(mins) + " minutes")
-
-m = int(input("Enter minutes: "))
-convert_minutes(m)
